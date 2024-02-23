e3138131a7b02ea209d7ce63f68dec0f97d703fa
Mario Bergeron <grouby177@gmail.com>
2024-02-23 17:30:49 -0500

    hailo-8: Update code to convert tflite models.

--- hailo-8/hailo_ai_sw_suite_docker/shared_with_docker/deploy_models.sh
+++ hailo-8/hailo_ai_sw_suite_docker/shared_with_docker/deploy_models.sh
@@ -1,9 +1,10 @@
+# ONNX models
 model_palm_detector_v0_07=("palm_detection_v0_07","models/palm_detection_v0_07/model_float32.onnx",256)
 model_palm_detector_v0_10_lite=("palm_detection_lite","models/palm_detection_lite/model_float32.onnx",192)
-model_palm_detector_v0_10_full=("palm_detection_full","models/palm_detection_lite/model_float32.onnx",192)
+model_palm_detector_v0_10_full=("palm_detection_full","models/palm_detection_full/model_float32.onnx",192)
 model_hand_landmark_v0_07=("hand_landmark_v0_07","models/hand_landmark_v0_07/model_float32.onnx",256)
 model_hand_landmark_v0_10_lite=("hand_landmark_lite","models/hand_landmark_lite/model_float32.onnx",224)
-model_hand_landmark_v0_10_full=("hand_landmark_full","models/hand_landmark_lite/model_float32.onnx",224)
+model_hand_landmark_v0_10_full=("hand_landmark_full","models/hand_landmark_full/model_float32.onnx",224)
 model_list=(
 	model_palm_detector_v0_07[@]
 	model_palm_detector_v0_10_lite[@]
@@ -12,10 +13,16 @@ model_list=(
 	model_hand_landmark_v0_10_lite[@]
 	model_hand_landmark_v0_10_full[@]
 )
+# TFLite models
+model_palm_detector_v0_10_lite=("palm_detection_lite","models/palm_detection_lite.tflite",192)
+model_palm_detector_v0_10_full=("palm_detection_full","models/palm_detection_full.tflite",192)
+model_hand_landmark_v0_10_lite=("hand_landmark_lite","models/hand_landmark_lite.tflite",224)
+model_hand_landmark_v0_10_full=("hand_landmark_full","models/hand_landmark_full.tflite",224)
 model_list=(
-	model_palm_detector_v0_07[@]
 	model_palm_detector_v0_10_lite[@]
 	model_palm_detector_v0_10_full[@]
+	model_hand_landmark_v0_10_lite[@]
+	model_hand_landmark_v0_10_full[@]
 )
 model_count=${#model_list[@]}
 #echo $model_count
--- hailo-8/hailo_ai_sw_suite_docker/shared_with_docker/hailo_flow.py
+++ hailo-8/hailo_ai_sw_suite_docker/shared_with_docker/hailo_flow.py
@@ -12,7 +12,7 @@ ap.add_argument('-b', '--blaze'      , type=str,  default="hand", help="Blaze ap
 ap.add_argument('-n', '--name'       , type=str,  default="palm_detection_lite", help="Model name. Default is 'palm_detection_lite'")
 ap.add_argument('-m', '--model'      , type=str,  default="models/palm_detection_lite/model_float32.onnx", help="Model file (ONNX format). Default is 'models/palm_detection_lite/model_float32.onnx'")
 ap.add_argument('-r', '--resolution' , type=int,  default=192, help="Input resolution.  Default is 192 for 192x192.")
-ap.add_argument('-p', '--process'    , type=str,  default="all", help="Command seperated list of processes to run ( 'inspect', 'parse,optimize,compile' ). Default is 'all'")
+ap.add_argument('-p', '--process'    , type=str,  default="all", help="Command seperated list of processes to run ( 'inspect', 'profile', 'all'=='parse,optimize,compile' ). Default is 'all'")
 
 
 args = ap.parse_args()  
@@ -36,65 +36,132 @@ if ("inspect" in args.process):
     runner = ClientRunner(hw_arch=args.arch)
     model_path = args.model
     model_name = args.name
+    
+    file_name, model_type = os.path.splitext(model_path)
+    if model_type == ".tflite":
+        hn, npz = runner.translate_tf_model(model_path,model_name)
+    elif model_type == ".onnx":
+        hn, npz = runner.translate_onnx_model(model_path,model_name)
+    else:
+        print("[ERROR] Unknown model type : ",model_type)
 
-    hn, npz = runner.translate_onnx_model(model_path,model_name)
-
-if ("parse" in args.process or args.process == "all"):
+if ("parse" in args.process or "profile" in args.process or args.process == "all"):
 
     runner = ClientRunner(hw_arch=args.arch)
     model_path = args.model
     model_name = args.name
 
-    if args.name == "palm_detection_v0_07":
-        start_node_names = ['input']
-        assert (args.resolution==256), "palm_detection_v0_07 resolution should be 256"
-        net_input_shapes={'input': [1, 3, args.resolution, args.resolution]}
-        #end_node_names = ['regressors','classificators']
-        end_node_names = ['Conv__533', 'Conv__544', 'Conv__551', 'Conv__532', 'Conv__543', 'Conv__550']
-        # Conv__533 [1x6x8x8]   =transpose=> [1x8x8x6]   =reshape=> [1x384x1]  \\
-        # Conv__544 [1x2x16x16] =transpose=> [1x16x16x2] =reshape=> [1x512x1]    => [1x2944x1]
-        # Conv__551 [1x2x32x32] =transpose=> [1x32x23x2] =reshape=> [1x2048x1]  //
-        #
-        # Conv__532 [1x108x8x8]  =transpose=> [1x8x8x108]  =reshape=> [1x384x18]   \\
-        # Conv__543 [1x36x16x16] =transpose=> [1x16x16x36] =reshape=> [1x512x18]    => [1x2944x18]
-        # Conv__550 [1x36x32x32] =transpose=> [1x32x32x36] =reshape=> [1x2048x1]  //
-    elif args.name == "hand_landmark_v0_07":
-        start_node_names = ['input_1']
-        assert (args.resolution==256), "hand_landmark_v0_07 resolution should be 256"
-        net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
-        end_node_names = ['ld_21_3d','output_handflag','output_handedness']
-    elif args.name == "palm_detection_lite" or args.name == "palm_detection_full":
-        start_node_names = ['input_1']
-        assert (args.resolution==192), "palm_detection_lite/full resolution should be 192"
-        net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
-        #end_node_names = ['Identity','Identity_1']
-        end_node_names = ['Conv__410', 'Conv__412','Conv__409','Conv__411']
-        # Conv__410 [1x2x24x24] =transpose=> [1x24x24x2] =reshape=> [1x1152x1] \\
-        #                                                                        => [1x2016x1]
-        # Conv__412 [1x6x12x12] =transpose=> [1x12x12x6] =reshape=> [1x864x1]  //
-        #
-        # Conv__409 [1x36x24x24]  =transpose=> [1x24x24x36]  =reshape=> [1x1152x18] \\
-        #                                                                             => [1x2016x18]
-        # Conv__411 [1x108x12x12] =transpose=> [1x12x12x108] =reshape=> [1x864x18]  //
-    elif args.name == "hand_landmark_lite" or args.name == "hand_landmark_full":
-        start_node_names = ['input_1']
-        assert (args.resolution==224), "hand_landmark_lite/full resolution should be 192"
-        net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
-        end_node_names = ['Identity','Identity_1','Identity_2','Identity_3']
+    file_name, model_type = os.path.splitext(model_path)
+    if model_type == ".tflite":
+    
+        if args.name == "palm_detection_v0_07":
+            start_node_names = ['input']
+            assert (args.resolution==256), "palm_detection_v0_07 resolution should be 256"
+            #end_node_names = ['classificators','regressors']
+            #end_node_names = ['classificator_8','classificator_16', 'classificator_32','regressor_8','regressor_16', 'regressor_32']
+            end_node_names = ['activation_41', 'activation_23', 'activation_31', 'activation_39']
+        elif args.name == "hand_landmark_v0_07":
+            start_node_names = ['input_1']
+            assert (args.resolution==256), "hand_landmark_v0_07 resolution should be 256"
+            end_node_names = ['ld_21_3d','output_handflag','output_handedness']
+        elif args.name == "palm_detection_lite" or args.name == "palm_detection_full":
+            start_node_names = ['input_1']
+            assert (args.resolution==192), "palm_detection_lite/full resolution should be 192"
+            #end_node_names = ['Identity','Identity_1']
+            #hailo_sdk_common.hailo_nn.exceptions.UnsupportedModelError: 1D form is not supported in layer Identity_1 of type ConcatLayer.
+            #end_node_names = ['model_1/model/reshaped_classifier_palm_16/Reshape', 'model_1/model/reshaped_classifier_palm_8/Reshape','model_1/model/reshaped_regressor_palm_16/Reshape','model_1/model/reshaped_regressor_palm_8/Reshape']
+            #hailo_sdk_client.model_translator.exceptions.ParsingWithRecommendationException: Parsing failed. The errors found in the graph are:
+            # UnsupportedShuffleLayerError in op model_1/model/reshaped_classifier_palm_8/Reshape: Failed to determine type of layer to create in node model_1/model/reshaped_classifier_palm_8/Reshape (RESHAPE)
+            # UnsupportedShuffleLayerError in op model_1/model/reshaped_regressor_palm_8/Reshape: Failed to determine type of layer to create in node model_1/model/reshaped_regressor_palm_8/Reshape (RESHAPE)
+            # UnsupportedShuffleLayerError in op model_1/model/reshaped_classifier_palm_16/Reshape: Failed to determine type of layer to create in node model_1/model/reshaped_classifier_palm_16/Reshape (RESHAPE)
+            # UnsupportedShuffleLayerError in op model_1/model/reshaped_regressor_palm_16/Reshape: Failed to determine type of layer to create in node model_1/model/reshaped_regressor_palm_16/Reshape (RESHAPE)
+            #Please try to parse the model again, using these end node names: model_1/model/classifier_palm_16_NO_PRUNING/BiasAdd;model_1/model/classifier_palm_16_NO_PRUNING/Conv2D;model_1/model/classifier_palm_16_NO_PRUNING/BiasAdd/ReadVariableOp/resource1, model_1/model/classifier_palm_8_NO_PRUNING/BiasAdd;model_1/model/classifier_palm_8_NO_PRUNING/Conv2D;model_1/model/classifier_palm_8_NO_PRUNING/BiasAdd/ReadVariableOp/resource1, model_1/model/regressor_palm_16_NO_PRUNING/BiasAdd;model_1/model/regressor_palm_16_NO_PRUNING/Conv2D;model_1/model/regressor_palm_16_NO_PRUNING/BiasAdd/ReadVariableOp/resource1, model_1/model/regressor_palm_8_NO_PRUNING/BiasAdd;model_1/model/regressor_palm_8_NO_PRUNING/Conv2D;model_1/model/regressor_palm_8_NO_PRUNING/BiasAdd/ReadVariableOp/resource1
+            end_node_names = [
+                'model_1/model/classifier_palm_16_NO_PRUNING/BiasAdd;model_1/model/classifier_palm_16_NO_PRUNING/Conv2D;model_1/model/classifier_palm_16_NO_PRUNING/BiasAdd/ReadVariableOp/resource1', 
+                'model_1/model/classifier_palm_8_NO_PRUNING/BiasAdd;model_1/model/classifier_palm_8_NO_PRUNING/Conv2D;model_1/model/classifier_palm_8_NO_PRUNING/BiasAdd/ReadVariableOp/resource1', 
+                'model_1/model/regressor_palm_16_NO_PRUNING/BiasAdd;model_1/model/regressor_palm_16_NO_PRUNING/Conv2D;model_1/model/regressor_palm_16_NO_PRUNING/BiasAdd/ReadVariableOp/resource1', 
+                'model_1/model/regressor_palm_8_NO_PRUNING/BiasAdd;model_1/model/regressor_palm_8_NO_PRUNING/Conv2D;model_1/model/regressor_palm_8_NO_PRUNING/BiasAdd/ReadVariableOp/resource1']
+        elif args.name == "hand_landmark_lite" or args.name == "hand_landmark_full":
+            start_node_names = ['input_1']
+            assert (args.resolution==224), "hand_landmark_lite/full resolution should be 192"
+            end_node_names = ['Identity','Identity_1','Identity_2','Identity_3']
+        else:
+            start_node_names = []
+            end_node_names = []
+
+        hn, npz = runner.translate_tf_model(
+            model_path, 
+            model_name, 
+            start_node_names=start_node_names,
+            end_node_names=end_node_names)
+    
+    elif model_type == ".onnx":
+
+        if args.name == "palm_detection_v0_07":
+            start_node_names = ['input']
+            assert (args.resolution==256), "palm_detection_v0_07 resolution should be 256"
+            net_input_shapes={'input': [1, 3, args.resolution, args.resolution]}
+            #end_node_names = ['regressors','classificators']
+            end_node_names = ['Conv__533', 'Conv__544', 'Conv__551', 'Conv__532', 'Conv__543', 'Conv__550']
+            # Conv__533 [1x6x8x8]   =transpose=> [1x8x8x6]   =reshape=> [1x384x1]  \\
+            # Conv__544 [1x2x16x16] =transpose=> [1x16x16x2] =reshape=> [1x512x1]    => [1x2944x1]
+            # Conv__551 [1x2x32x32] =transpose=> [1x32x23x2] =reshape=> [1x2048x1]  //
+            #
+            # Conv__532 [1x108x8x8]  =transpose=> [1x8x8x108]  =reshape=> [1x384x18]   \\
+            # Conv__543 [1x36x16x16] =transpose=> [1x16x16x36] =reshape=> [1x512x18]    => [1x2944x18]
+            # Conv__550 [1x36x32x32] =transpose=> [1x32x32x36] =reshape=> [1x2048x1]  //
+        elif args.name == "hand_landmark_v0_07":
+            start_node_names = ['input_1']
+            assert (args.resolution==256), "hand_landmark_v0_07 resolution should be 256"
+            net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
+            end_node_names = ['ld_21_3d','output_handflag','output_handedness']
+        elif args.name == "palm_detection_lite" or args.name == "palm_detection_full":
+            start_node_names = ['input_1']
+            assert (args.resolution==192), "palm_detection_lite/full resolution should be 192"
+            net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
+            #end_node_names = ['Identity','Identity_1']
+            end_node_names = ['Conv__410', 'Conv__412','Conv__409','Conv__411']
+            # Conv__410 [1x2x24x24] =transpose=> [1x24x24x2] =reshape=> [1x1152x1] \\
+            #                                                                        => [1x2016x1]
+            # Conv__412 [1x6x12x12] =transpose=> [1x12x12x6] =reshape=> [1x864x1]  //
+            #
+            # Conv__409 [1x36x24x24]  =transpose=> [1x24x24x36]  =reshape=> [1x1152x18] \\
+            #                                                                             => [1x2016x18]
+            # Conv__411 [1x108x12x12] =transpose=> [1x12x12x108] =reshape=> [1x864x18]  //
+        elif args.name == "hand_landmark_lite" or args.name == "hand_landmark_full":
+            start_node_names = ['input_1']
+            assert (args.resolution==224), "hand_landmark_lite/full resolution should be 192"
+            net_input_shapes={'input_1': [1, 3, args.resolution, args.resolution]}
+            end_node_names = ['Identity','Identity_1','Identity_2','Identity_3']
+        else:
+            start_node_names = []
+            end_node_names = []
+
+        hn, npz = runner.translate_onnx_model(
+            model_path, 
+            model_name, 
+            start_node_names=start_node_names,
+            net_input_shapes=net_input_shapes,
+            end_node_names=end_node_names)
+
     else:
-        start_node_names = []
-        end_node_names = []
-
-   
-    hn, npz = runner.translate_onnx_model(
-        model_path, 
-        model_name, 
-        start_node_names=start_node_names,
-        net_input_shapes=net_input_shapes,
-        end_node_names=end_node_names)
-
+        print("[ERROR] Unknown model type : ",model_type)
+        
     hailo_model_har_name = f'{model_name}_hailo_model.har'
     runner.save_har(hailo_model_har_name)
+
+#
+# Profile
+# Reference: ...
+#
+
+if ("profile" in args.process):
+
+   model_name = args.name
+   # hn, npz ... from previous step ...
+   
+   detailed, summary = runner.profile_hn_model(fps=120)   
+
 
 #
 # Optimization
@@ -118,7 +185,7 @@ if ("optimize" in args.process or args.process == "all"):
     # Otherwise we would have to normalize it before using it)
 
     # Batch size is 8 by default
-    #alls = 'normalization1 = normalization([255.0 255.0 255.0], [0.0 0.0 0.0])\n'
+    #alls = 'normalization1 = normalization([255.0, 255.0, 255.0], [0.0, 0.0, 0.0])\n'
 
     # Load the model script to ClientRunner so it will be considered on optimization
     #runner.load_model_script(alls)
